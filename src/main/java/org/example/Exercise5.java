523b656b7cdd29c02f12a14dc53d66be0d4294b4
Hendrik Schick <hendrikjschick@gmail.com>
2024-01-02 00:53:49 +0100

    spiral transfer

--- src/main/java/org/example/Exercise5.java
+++ src/main/java/org/example/Exercise5.java
@@ -8,41 +8,58 @@ import java.util.List;
 public class Exercise5 {
 
    public void run() {
-      Integer[][] inputs = {{ 1, 2, 3, 4},
+      Integer[][] inputs = {
+            { 1, 2, 3, 4},
             {12, 13, 14, 5},
             {11, 16, 15, 6},
-            {10, 9, 8, 7}};
+            {10, 9, 8, 7}
+      };
 
-      String[][] letterPairs = {{"AB", "BC", "CD", "DE"}, {"JK", "KL", "LM", "EF"}, {"IJ", "HI", "GH", "FG"}};
+      String[][] letterPairs = {
+            {"AB", "BC", "CD", "DE"},
+            {"JK", "KL", "LM", "EF"},
+            {"IJ", "HI", "GH", "FG"}
+      };
 
-      var res1 = spiralTraversal(inputs);
-      res1.forEach(System.out::println);
+//      var res1 = spiralTraversal(inputs);
+//      res1.forEach(System.out::println);
+//      System.out.println();
+      var res2 = spiralTraversal(letterPairs);
+      res2.forEach(System.out::println);
    }
 
    <T> List<T> spiralTraversal(T[][] twoDimArray) {
       var traversedList = new ArrayList<T>();
+      var inc = 0;
       for (int i = 0; i <= (twoDimArray.length - 1) / 2; i++) {
+         var k = 1;
+         var l = twoDimArray[0].length - 1;
+         var m = twoDimArray.length - 2;
+
          // left to right upper row
-         for (int j = 0; j <= twoDimArray[0].length - 1; j++) {
+         for (int j = 0 + inc; j <= twoDimArray[0].length - 1 - inc; j++) {
             traversedList.add(twoDimArray[i][j]);
          }
 
          // top to bottom right column
-         for (int k = 1; k <= twoDimArray.length - 2; k ++) {
-            traversedList.add(twoDimArray[k][twoDimArray.length - 1]);
+         for (; k + inc <= twoDimArray.length - 2 - inc; k ++) {
+            traversedList.add(twoDimArray[k + inc][twoDimArray[0].length - 1 - inc]);
          }
 
          // right to left bottom row
-         for (int l = twoDimArray[0].length - 1; l >= 0; l--) {
-            traversedList.add(twoDimArray[twoDimArray.length - 1][l]);
+         for (; l - inc >= 0 + inc; l--) {
+            if (twoDimArray.length % 2 == 1 && inc == twoDimArray.length / 2) {
+               break;
+            }
+            traversedList.add(twoDimArray[twoDimArray.length - 1 - inc][l - inc]);
          }
 
          // bottom to top left column
-         for (int m = twoDimArray.length - 2; m >= 1; m--) {
-            traversedList.add(twoDimArray[m][0]);
+         for (; m - inc >= 1 + inc; m--) {
+            traversedList.add(twoDimArray[m - inc][i]);
          }
 
-         break;
+         inc++;
       }
       return traversedList;
    }
